cb12d9f929b0bed940745d8afba036767a4a1ab8
justinyeh1995 <justinyeh1995@gmail.com>
2022-12-04 21:49:37 -0600

    remove .sh

--- PeNDeS/create_plugin.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-echo Please enter the name of your plugin:
-read id
-webgme node ./node_modules/webgme-cli/bin/webgme new plugin --language Python $id
-webgme npm i
--- PeNDeS/create_seed.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-echo Please enter the name of your seed:
-read id
-echo Please enter the name of the input project file:
-read input
-webgme node ./node_modules/webgme-cli/bin/webgme new seed -f $input $id
